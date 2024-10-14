d1abb563dfe2c98b3eb20a6ad36c2d935c28c003
HieuPham1607 <phamhiuhiu01@gmail.com>
2024-10-14 10:40:08 +0700

    ls22

---
+++ ls22/Lab1-1/Lab01-ex2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lab01_ex2
+/// <summary>
+/// Author : Phạm Hiệu
+/// Date : 14/10/2024
+/// Chương trình C# đầu tiên
+/// </summary>
+//Viết chương trình thực hiện các yêu cầu sau
+//- Khai báo các biến để lưu trữ thông tin của Nhân Viên bao gồm:
+//o Mã Nhân viên
+//o Họ tên nhân viên
+//o Ngày sinh
+//o Địa chỉ
+//o Điện thoại
+//o Email
+//- Gán giá trị cho các biến
+//- Hiển thị trên màn hình thông tin của nhân viên
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Xuất có unicode
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Viết chương trình thực hiện yêu cầu sau");
+            //khai báo biến
+            string maNV;
+            string HoVaTen;
+            DateTime ngaysinh;
+            string diaChi;
+            string dienThoai;
+            string email;
+
+            //Gán giá trị
+            maNV = "NV001";
+            HoVaTen = "Phạm Quốc Hiệu";
+            ngaysinh = DateTime.Now;
+            diaChi = "Nhị Khê";
+            dienThoai = "0969670959";
+            email = "Phamhiuhiu01@gmail.com";
+
+
+            //Hiển thị thông tin
+            Console.WriteLine("===== Thông tin cá nhân");
+            Console.WriteLine("Mã nhân viên",maNV);
+            Console.WriteLine("Họ tên nhân viên",HoVaTen);
+            Console.WriteLine("Ngày sinh",ngaysinh);
+            Console.WriteLine("Địa chỉ",diaChi);
+            Console.WriteLine("Điện thoại", dienThoai);
+            Console.WriteLine("email",email);
+        }
+    }
+}
---
+++ ls22/Lab1-1/Lab1-1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab1_1
+/// <summary>
+/// Author : Phạm Hiệu
+/// Date : 14/10/2024
+/// Chương trình C# đầu tiên
+/// In ra nội dung thông tin giới thiệu về bản thân
+/// </summary>
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Xuất có unicode
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Lệnh in ra màn hình dòng văn bản sau đó xuống dòng
+            Console.WriteLine("Họ và tên : Phạm Quốc Hiệu!");
+            // Lệnh in ra có định dạng
+            Console.WriteLine("Ngày sinh: {0}", "16/07/2003");
+            Console.Write("Địa chỉ: Tầng 6, Số 25 Phố Vũ Ngọc Phan  \n");
+            Console.WriteLine("Điện thoại: 0969670959");
+            Console.WriteLine("Email:Phamhiuhiu01@gmail.com");
+
+        }
+    }
+}
+
