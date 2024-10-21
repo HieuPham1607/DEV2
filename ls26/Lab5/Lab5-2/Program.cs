8c6a664b09a68976d38ece2f8e0560c09a421e79
HieuPham1607 <phamhiuhiu01@gmail.com>
2024-10-21 11:25:10 +0700

    ls26

---
+++ ls26/Lab5/Lab5-2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Lab5_2
+///<summary>
+///Chương trình minh họa một số thao tác trên mảng 2 chiều
+///</summary>
+/// <param name = "args" ></ param >
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            int[,] arrays = {
+                {4, 6, 9 },
+                {2, 4, 5 },
+                {9, 2, 6 },
+                {1, 6, 3 }
+            };
+            //Duyệt mảng và in
+            prinArray(arrays);
+            prinArrayXY(arrays);
+        }
+        static void prinArray(int[,] arrays) {
+            Console.WriteLine("Cac phan tu trong mang");
+            for (int i = 0; i <= arrays.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arrays.GetUpperBound(1); j++) 
+                {
+                    Console.Write(" {0} ", arrays[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void prinArrayXY(int[,] arrays) 
+        {
+            Console.WriteLine("Cac phan tu lon nhat trn hang");
+            for (int i = 0; i <= arrays.GetUpperBound(0); i++)
+            {
+                int max = arrays[i, 0];
+                for (int j = 1; j <= arrays.GetUpperBound(1); j++)
+                {
+                    if (arrays[i, j] > max)
+                        max = arrays[i, j];
+                }
+                Console.WriteLine("Hang {0}:{1}", i, max);
+            } 
+                
+        }
+    }
+}
---
+++ ls26/Lab5/Lab5/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Lab5
+    ///<summary>
+    ///Một số thao tác với mảng một chiều
+    ///</summary>
+    ///<param name = "args" ></ param >
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Khai báo và khởi tạo mảng 1 chiều
+            int[] m = { 1,2,3,3,2,1 };
+            //duyệt mảng và ỉn a dữ liệu
+            Console.Write("Cac phan tu cua mang");
+            for (int i = 0; i < m.Length; i++) { 
+                Console.Write(" {0} ", m[i]);
+            }
+            //Tìm phần tử lớn nhất
+            int max = m[0];
+            for (int i = 1; i < m.Length; i++) { 
+                if (max < m[i])
+                    max = m[i];
+            }
+            Console.WriteLine("\nPhan tu lon nhat:" + max);
+            //Kiểm tra mảng có đối xứng không?
+            bool kt = true;
+            for (int i = 0; i < m.Length / 2; i++) { 
+                if (m[i] != m[m.Length - 1 - i])
+                {
+                    kt = false;
+                    break;
+                }
+            }
+            if (kt)
+                Console.WriteLine("Mang doi xung");
+            else
+                Console.WriteLine("Mang khong doi xung");
+        }
+    }
+}
