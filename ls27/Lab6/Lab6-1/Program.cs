34e9204a3f4714a815fe6af52ec41e83f3fb20b2
HieuPham1607 <phamhiuhiu01@gmail.com>
2024-10-22 10:21:10 +0700

    ls27

---
+++ ls27/Lab6/Lab6-1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace Lab6_1
+{
+    internal class Program
+    {
+        //tạo lớp Product
+        class Product
+        {
+            //khai báo trường
+            string name;
+            double cost;
+            int onhand;
+            //constructor
+            public Product(string n, double c, int h)
+            {
+                name = n;
+                cost = c;
+                onhand = h;
+            }
+            //ghi đè phương thức
+            public override string ToString()
+            {
+                return String.Format("{0,-10}Cost: {1,6:C} On hand: {2}", name,
+                cost, onhand);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //tạo ArrayList
+            ArrayList inv = new ArrayList();
+            //thêm phần tử vào dánh sách
+            inv.Add(new Product("A", 5.9, 3));
+            inv.Add(new Product("B", 8.2, 2));
+            inv.Add(new Product("C", 3.5, 4));
+            inv.Add(new Product("D", 1.8, 8));
+            //in ra danh sách phần tử
+            Console.WriteLine("Product list:");
+            foreach (Product i in inv)
+            {
+                Console.WriteLine(" " + i);
+            }
+        }
+
+
+    }
+}
---
+++ ls27/Lab6/Lab6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Arraylist");
+            //Khởi tạo
+            ArrayList arraylist = new ArrayList();
+
+            //Thêm các phân tử
+            //Add
+            arraylist.Add(111);
+            arraylist.Add("Hiệu Hiệu");
+
+            // AddRange
+            string[] strs = { "Tùng" , "Cúc" , "Trúc" , "Mai" };
+            arraylist.AddRange(strs);
+
+            //In mảng 
+            printArray(arraylist);
+
+            //Insert
+            arraylist.Insert(0, "Devmaster");
+            printArray(arraylist);
+
+            // remove
+            arraylist.Remove("Devmaster");
+            printArray( arraylist);
+
+            arraylist.RemoveAt(1);
+            printArray(arraylist);
+
+            //sort
+            arraylist.AddRange(strs);
+            printArray(arraylist);
+            arraylist.Sort();
+            printArray(arraylist);
+        }
+
+        static void printArray(ArrayList arraylist)
+        {
+            Console.WriteLine("Danh sách các phần tử");
+            foreach (var item in arraylist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
